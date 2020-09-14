f80b3422f2feeb9926211a252adbc1bf498798ac
Melinda <melinda.sava.td@gmail.com>
2020-09-14 23:05:36 +0200

    solved Chessboard but in a different way than expected

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
-
+// Looping a triangle
 let hash = "#";
 for (i = 0; i < 7; i++) {
     console.log(hash);
     hash += "#";
 }
+
+// FizzBuzz
+
+for (let num = 1; num <= 100; num++) {
+
+    if (num % 3 == 0 && num % 5 == 0) {
+        console.log('FizzBuzz');
+    } else if (num % 3 == 0) {
+        console.log('Fizz');
+    } else if (num % 5 == 0) {
+        console.log('Buzz');
+    } else {
+        console.log(num);
+    }
+}
+
+// Chessboard
+
+let black = "#";
+let row1 = '';
+let row2 = '';
+let chess = "";
+console.log(black);
+
+
+for (j = 0; j < 4; j++) {
+    row1 += black + " ";
+}
+
+for (i = 0; i < 4; i++) {
+    row2 += " " + black;
+}
+
+for (k = 0; k < 4; k++) {
+    chess += row1 + '\n' + row2 + '\n';
+}
+console.log(chess)
